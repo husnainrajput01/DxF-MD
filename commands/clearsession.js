23ee4642be64a28372f4b75105ef12fcc43d074c
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2026-02-09 15:21:23 +0500

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -8,8 +8,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363420537894461@newsletter',
+            newsletterName: '𝕯𝖆𝖓𝖌𝖊𝖗𝕏ꜰᴇᴀʀ MD',
             serverMessageId: -1
         }
     }
